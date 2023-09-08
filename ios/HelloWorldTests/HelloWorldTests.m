02c51cf1484a787b4338d61f652fb732ff501269
Mopharr <olamiquadri1@gmail.com>
2023-09-08 01:33:37 +0100

    uda

--- ios/HelloWorldTests/HelloWorldTests.m
+++ ios/HelloWorldTests/HelloWorldTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface HelloWorldTests : XCTestCase
+@interface LasuStaffConnerTests : XCTestCase
 
 @end
 
-@implementation HelloWorldTests
+@implementation LasuStaffConnerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
